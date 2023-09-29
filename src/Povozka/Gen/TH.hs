c6e9f70f5583e39ae449ceca83b925c7171ae769
Timur Aminev <ablearthy@gmail.com>
2023-09-29 18:41:51 +0300

    Start implementing generator for `Binary`

--- src/Povozka/Gen/TH.hs
+++ src/Povozka/Gen/TH.hs
@@ -1,7 +1,8 @@
 {-# LANGUAGE OverloadedRecordDot #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TemplateHaskell #-}
 
-module Povozka.Gen.TH (generateConstructor, generateType, generateTypeFamily, pprint) where
+module Povozka.Gen.TH (generateConstructor, generateType, generateTypeFamily, generateBinaryInstanceForConstructor, pprint, runQ) where
 
 import Language.Haskell.TH
 
@@ -9,6 +10,8 @@ import Data.Text qualified as T
 import Povozka.Gen.Model
 
 import Data.Maybe (fromMaybe)
+import Data.List (foldl')
+import Data.Map.Strict qualified as M
 
 text2name :: T.Text -> Name
 text2name = mkName . T.unpack
@@ -29,10 +32,10 @@ generateConstructor comb =
   where
     constrName = text2name comb.constr
 
-    fields = map (\(fname, ftype) -> (text2name fname, defaultBang, intermediateTypeToType (tr_ ftype))) comb.fields
+    fields = map (\(fname, ftype) -> (text2name fname, defaultBang, (tr_ ftype))) comb.fields
       where
-        tr_ (Field x) = x
-        tr_ (Conditional _ _ x) = x
+        tr_ (Field x) = intermediateTypeToType x
+        tr_ (Conditional _ _ x) = AppT (ConT $ mkName "Maybe") $ intermediateTypeToType x
 
     defaultBang = Bang NoSourceUnpackedness SourceStrict
 
@@ -52,3 +55,38 @@ generateTypeFamily combs@(c : _) = a : b
   where
     a = generateType c.typeName combs
     b = map generateConstructor combs
+
+generateBinaryInstanceForConstructor :: Combinator -> Q Dec
+generateBinaryInstanceForConstructor comb = do
+  g <- bodyGet
+  pure $ InstanceD Nothing [] instanceName [FunD (mkName "get") [Clause [] (NormalB g) []]]
+  where
+    instanceName = AppT (ConT (mkName "Data.Binary.Binary")) (ConT combName)
+    combName = text2name comb.constr
+
+    bodyGet = do
+        (names, stmts) <- generateGetParams comb.fields
+        thePure <- runQ [|pure|]
+        let r = foldl' AppE (ConE combName) names
+        let lastStmt = NoBindS $ AppE thePure r
+        pure $ DoE Nothing (reverse (lastStmt:stmts))
+
+generateGetParams :: [(T.Text, Field)] -> Q ([Exp], [Stmt])
+generateGetParams = go (mempty, [], [])
+  where
+    go :: (M.Map T.Text Name, [Exp], [Stmt]) -> [(T.Text, Field)] -> Q ([Exp], [Stmt])
+    go (_, a, b) [] = pure (reverse a, b)
+    go (s, names, stmts) ((name, Field _):rest) = do
+        nn <- newName (T.unpack name)
+        let newStmt = BindS (VarP nn) (VarE (mkName "Data.Binary.get"))
+        go (M.insert name nn s, VarE nn:names, newStmt:stmts) rest
+    go (s, names, stmts) ((name, Conditional v midx _):rest) = do
+        nn <- newName (T.unpack name)
+        l <- tr_ midx
+        let newStmt = BindS (VarP nn) (AppE (AppE (VarE (mkName "Data.Binary.tlHandleOpt")) (VarE (s M.! v))) l)
+        go (M.insert name nn s, VarE nn:names, newStmt:stmts) rest
+      where
+        tr_ Nothing = [| Nothing |]
+        tr_ (Just x) = do
+            let r = pure $ LitE $ IntegerL (fromIntegral x)
+            [| Just $r |]
