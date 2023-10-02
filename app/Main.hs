f65635ec3b9439ccd36eb388d8c92f80e215410c
Timur Aminev <ablearthy@gmail.com>
2023-10-02 22:49:15 +0300

    Create sample executable

--- app/Main.hs
+++ app/Main.hs
@@ -1,8 +1,39 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
-import qualified MyLib (someFunc)
+import Control.Monad (forM_)
+import Data.ByteString.Lazy.Char8 qualified as BS
+import Data.Map.Strict qualified as M
+import Data.Text qualified as T
+import Povozka.Gen.FromSchema (extractFunctions, extractTypes)
+import Povozka.Gen.TH (generateBinaryInstanceForConstructor, generateBinaryInstanceForTypeFamily', generateFunction, generateTypeFamily, pprint, runQ)
+import Povozka.Parser (parseTL, runAlex)
 
 main :: IO ()
 main = do
-  putStrLn "Hello, Haskell!"
-  MyLib.someFunc
+  contents <- BS.readFile "./test.tl"
+  case runAlex contents parseTL of
+    Left e -> print e
+    Right schema -> do
+      let m = extractTypes schema
+      let f = extractFunctions schema
+      forM_ (M.toList m) $ \(t, constrs) -> do
+        forM_ (M.toList constrs) $ \(rawConstrName, comb) -> do
+          ex <- runQ (generateBinaryInstanceForConstructor comb)
+          putStrLn $ pprint ex
+        let expr = generateTypeFamily (M.elems constrs)
+        ex <- runQ (generateBinaryInstanceForTypeFamily' (M.elems constrs))
+        putStrLn $ pprint expr
+        putStrLn $ pprint ex
+      forM_ f $ \r -> do
+        ex <- runQ (generateFunction r)
+        putStrLn $ pprint ex
+
+-- let constructors = constrDecls schema
+-- let ctx = obtainContext constructors
+-- forM_ constructors $ \(Decl _ _ args _) -> do
+--   let terms = map (\(SimpleArg _ _ _ t) -> t) args
+--   let irs = map (term2intermediateType ctx) terms
+--   forM_ irs $ putStrLn <$> pprintIntermediateType
+-- print ctx
